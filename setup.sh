404ee7352ba06377dc9010409ec7f891eed5fa72
dattran <dattran2346@gmail.com>
2017-05-22 06:42:26 +0700

    Update setup

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ wget https://physionet.org/challenge/2017/training2017.zip
 unzip training2017
 wget https://physionet.org/challenge/2017/sample2017.zip
 mv sample/validation validation
+rm sample2017.zip
+rm training2017.zip
 unzip sample2017
 pip install -r requirements.txt
